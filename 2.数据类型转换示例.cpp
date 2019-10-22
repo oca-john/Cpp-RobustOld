0a51e8833a3b48d75642af67c790aed293dafc65
oca-john <40886062+oca-john@users.noreply.github.com>
2019-10-22 17:24:28 +0800

    Create 2.数据类型转换示例.cpp

---
+++ 2.数据类型转换示例.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int i;      //declaring integer variable
+	float f;    //declaring float variable
+	double d;   //declaring double variable
+	char c;     //declaring character variable
+	bool b, bl;     //decaring boolean variable;
+
+	//assigning values to these variables
+
+	i = 45;
+	f = 34.234;
+	d = 34.43242343;
+	c = 'g';
+	b = true;
+	bl = 5 < 4;
+
+	cout << "int : " << i << endl;
+	cout << "float : " << f << endl;
+	cout << "double : " << d << endl;
+	cout << "char : " << c << endl;
+	cout << "b : " << b << endl;
+	cout << "bl : " << bl << endl;
+	return 0;
+}
