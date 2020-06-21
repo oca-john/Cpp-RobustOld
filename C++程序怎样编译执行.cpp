d296aea15b650412ab7be3bc419c61369afdf708
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-21 21:43:32 +0800

    Update and rename C++程序怎样编译执行.md to C++程序怎样编译执行.cpp

--- C++程序怎样编译执行.cpp
+++ C++程序怎样编译执行.cpp
@@ -1,9 +1,9 @@
-## 1.编译：
+// 1.编译：
 // g++              编译工具  
 // source_file.cpp  源码  
 // -o               输出选项  
 // executable_file  可执行文件  
 `g++ source_file.cpp -o executable_file`
 
-## 2.执行：
+// 2.执行：
 `./executable_file`
