b651ecf594ad77f7fb19eb41b9033e5be4e17f2e
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-28 18:09:30 +0800

    Create C程序怎样编译执行.c

---
+++ CLearn/C程序怎样编译执行.c
@@ -0,0 +1,9 @@
+// 1.编译：
+// gcc              编译工具  
+// source_file.c    源码  
+// -o               输出选项  
+// executable_file  可执行文件  
+`gcc source_file.c -o executable_file`
+
+// 2.执行：
+`./executable_file`
