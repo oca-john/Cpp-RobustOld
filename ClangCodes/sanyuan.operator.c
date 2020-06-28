b73f041f1862184c3d42cf84e1216acdf552f4f9
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-28 22:15:09 +0800

    Add files via upload

---
+++ ClangCodes/sanyuan.operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// ternary operator, sanyuan yunsuan
+
+int main()
+{
+	int x = 20;	// define and int x as 20;
+	char y;		// define y as char;
+	y = (x == 2 ? 100 : 40);	// check whether x is 2, if yes y is 100, or y is 40;
+	printf("y value is %d\n",y);
+	return 0;
+}
+
