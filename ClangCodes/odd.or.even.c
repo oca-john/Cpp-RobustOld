5b2b3ac3a6e3c110947631cdeb886153647b5db7
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-28 18:21:11 +0800

    Add files via upload

---
+++ ClangCodes/odd.or.even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// judge the input num is odd or even
+
+int main()
+{
+	int a;
+	a = 132;
+	if (a % 2 == 0)
+	{
+		printf("this is an even.\n");
+	} else
+	{
+		printf("this is an odd.\n");
+	}
+	return 0;
+}
+
