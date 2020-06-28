0147784eb38046f690fcff787a023445521ee7f5
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-28 23:10:53 +0800

    Add files via upload

---
+++ ClangCodes/sum.num-for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// sum numbers from 1 to 100
+// for
+
+int main()
+{
+	int i;
+	for (i = 1; i <= 100; i ++)
+	{
+		printf("the value of sum from 1 to 100 is %d.\n", i);
+	}
+	return 0;
+}
+
