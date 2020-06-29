8a244144f5a0a4d5f8523278cfb73d3fde0f4271
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-29 22:17:15 +0800

    Update and rename 1.HelloWorld示例.cpp to 1.hello.c++.cpp

---
+++ CppCodes/1.hello.c++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// declare the "i/o stream lib" in the whole source file
+// like #include <stdio.n> in C lang
+
+int main()          // each cpp file has only one main() function
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;       // the main() function must return a result to system
+}
