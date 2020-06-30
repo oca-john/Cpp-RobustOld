474cb5c88e19bf0773f7dbbcc4b7581bfaf6d0f2
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 19:16:34 +0800

    Update 2.score.grade.cpp

--- CppCodes/2.score.grade.cpp
+++ CppCodes/2.score.grade.cpp
@@ -2,4 +2,12 @@
 
 // divide your score into different level
 
-int main()
+int main() {
+  int score = 57;         // 初始化一个分值
+  if (score >= 60) {      // 判断条件
+    std::cout << "Passed this time.\n";
+  } else {
+    std::cout << "Fail in this time.\n";
+  }
+  return 0;
+}
