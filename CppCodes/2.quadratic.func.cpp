260f339d77bed83950919c2648aa066a42517a42
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 18:58:19 +0800

    Update 2.quadratic.func.cpp

--- CppCodes/2.quadratic.func.cpp
+++ CppCodes/2.quadratic.func.cpp
@@ -3,4 +3,19 @@
 
 // quadratic function (二次函数)
 
-int main()
+int main() {
+  double a, b, c;
+  double root1, root2;
+  std::cout << "Enter a: ";   // 依次获得二次函数的三个参数
+  std::cin >> a;
+  std::cout << "Enter b: ";
+  std::cin >> b;
+  std::cout << "Enter c: ";
+  std::cin >> c;
+  
+  root1 = (-b + std::sqrt(b*b - 4*a*c))/(2*a);  // 构造求根公式，求解两个根
+  root2 = (-b - std::sqrt(b*b - 4*a*c))/(2*a);
+  std::cout << "Root 1 is " << root1 << "\n";
+  std::cout << "Root 2 is " << root2 << "\n";
+  return 0;
+}
