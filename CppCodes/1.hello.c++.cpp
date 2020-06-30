f4b138a379bbaf38bb1bcabbaa96690a7b10f6d1
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 18:25:22 +0800

    Update 1.hello.c++.cpp

--- CppCodes/1.hello.c++.cpp
+++ CppCodes/1.hello.c++.cpp
@@ -2,8 +2,7 @@
 // declare the "i/o stream lib" in the whole source file
 // like #include <stdio.n> in C lang
 
-int main()          // each cpp file has only one main() function
-{
+int main() {        // each cpp file has only one main() function
     std::cout << "hello world!" << std::endl;
     return 0;       // the main() function must return a result to system
 }
