73ff34dba86e5128f055e9f1c5199b5c70d81180
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 23:25:20 +0800

    Update 3.calc.square.area.cpp

--- CppCodes/3.calc.square.area.cpp
+++ CppCodes/3.calc.square.area.cpp
@@ -2,4 +2,13 @@
 
 // calculate area of aquare.
 
-int main()
+int main() {
+  int i = 0;              // 初始化
+  int square = 0;
+  while (i < 10) {        // 限定输出10以内结果
+    square = i * i;       // 计算公式
+    std::cout << i << ": " << square << "\n";   // 连续cout输出，变量和"string"分开
+    i++;
+  }
+  return 0:               // 返回0
+}
