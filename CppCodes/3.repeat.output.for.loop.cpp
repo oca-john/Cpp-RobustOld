708043b50fb9f1e7de386a11702997a3247e2391
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 23:19:34 +0800

    Update 3.repeat.output.for.loop.cpp

--- CppCodes/3.repeat.output.for.loop.cpp
+++ CppCodes/3.repeat.output.for.loop.cpp
@@ -2,4 +2,9 @@
 
 // use for loop to print out some strings
 
-int main()
+int main() {
+  for (int i = 0; i < 10; i++) {    // for loop 限定循环次数
+    std::cout "C++ is great.\n";
+  }
+  return 0;                         // 返回0
+}
