f897784541be6dc159a0618ca6bf8476c540da3f
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 18:29:16 +0800

    Update 2.temp.calc.cpp

--- CppCodes/2.temp.calc.cpp
+++ CppCodes/2.temp.calc.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+
+// change the temp from 'f' to 'c'.
+
+int main() {
+  double tempf = 83.0;
+  double tempc;
+  tempc = (tempf - 32) / 1.8;     // 核心就是这个转换公式，本质是函数
+  std::cout << "The temp is: " << tempc << " degress Celsius.\n";
+  return 0;
+}
