97d9ce41a3fc86b27ab941b0606100f86c09d5ac
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 21:33:07 +0800

    Update 2.get.weekday.cpp

--- CppCodes/2.get.weekday.cpp
+++ CppCodes/2.get.weekday.cpp
@@ -3,4 +3,35 @@
 // input a number, return the weekday of the number.
 // use 'switch()' and 'case'.
 
-int main()
+int main() {
+  int weekday;                      // 初始化变量
+  std::cout << "Please input a number.\n";
+  std::cin >> weekday;              // 用户输入该变量的值
+  switch (weekday) {                // switch主函数，和输入(本身就是条件)
+    case 1:                         // 逐个条件给出返回值
+      std::cout << "Monday.\n";
+      break;
+    case 2:
+      std::cout << "Tuesday.\n";
+      break;
+    case 3:
+      std::cout << "Wednesday.\n";
+      break;
+    case 4:
+      std::cout << "Thusday.\n";
+      break;
+    case 5:
+      std::cout << "Friday.\n";
+      break;
+    case 6:
+      std::cout << "Saturday.\n";
+      break;
+    case 7:
+      std::cout << "Sunday.\n";
+      break;
+    default:                        // 需给出不符合以上条件时的默认输出
+      std::cout << "No this day in a week.\n";
+      break;
+  }
+  return 0;                         // 返回0
+}
