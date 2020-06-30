19f66cc51f5e047b90592afcad45e6fba5d7e298
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-30 19:09:12 +0800

    Update 2.coinflip.rand.cpp

--- CppCodes/2.coinflip.rand.cpp
+++ CppCodes/2.coinflip.rand.cpp
@@ -5,3 +5,12 @@
 // 用rand随机数函数模拟抛硬币游戏
 
 int main() {
+  srand (time(NULL));     // 随机数发生器初始化
+  int coin = rand() % 2;  // 初始化coin状态，随机产生数字，测试对2取余的结果（只有0和1）
+  if (coin == 0) {
+    std::cout << "Heads.\n";  // 余0则输出正面，否则输出反面
+  } else {
+    std::cout << "Tails.\n";
+  }
+  return 0:
+}
