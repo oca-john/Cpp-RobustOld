2fd243d1f095150eb3cd79b1b48ab977bb85bbde
oca-john <40886062+oca-john@users.noreply.github.com>
2021-06-26 00:42:13 +0800

    Create 2.exchange.two.num.cpp

---
+++ CppCodes/2.exchange.two.num.cpp
@@ -0,0 +1,43 @@
+// with temp var
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int a = 5, b = 10, temp;
+ 
+    cout << "交换之前：" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+ 
+    temp = a;
+    a = b;
+    b = temp;
+ 
+    cout << "\n交换之后：" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+ 
+    return 0;
+}
+
+
+// without temp var
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    
+    int a = 5, b = 10;
+ 
+    cout << "交换之前：" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+ 
+    a = a + b;
+    b = a - b;
+    a = a - b;
+ 
+    cout << "\n交换之后：" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+ 
+    return 0;
+}
