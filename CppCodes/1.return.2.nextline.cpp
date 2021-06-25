fb35827e887297e7e5662bb52d3a8f680c19e0c0
oca-john <40886062+oca-john@users.noreply.github.com>
2021-06-26 00:33:58 +0800

    Create 1.return.2.nextline.cpp

---
+++ CppCodes/1.return.2.nextline.cpp
@@ -0,0 +1,21 @@
+// use `\n` as the line end;
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    cout << "Runoob \n";
+    cout << "Google \n";
+    cout << "Taobao";
+    return 0;
+}
+
+// use `endl` as the line end;
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    cout << "Runoob" << endl;
+    cout << "Google" << endl;
+    cout << "Taobao";
+    return 0;
+}
