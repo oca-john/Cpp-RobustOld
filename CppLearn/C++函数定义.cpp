3d745f08ba927886ce63ef4f227e254c7ab6164c
oca-john <40886062+oca-john@users.noreply.github.com>
2021-07-19 12:02:16 +0800

    Update C++函数定义.cpp

--- CppLearn/C++函数定义.cpp
+++ CppLearn/C++函数定义.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 using namespace std;
 
-
 // With int return, 返回值为 int 类型，末尾有 return int;
 int sayHi() {
 	cout << "Hello Oca!\n";
 	return 0;			// 返回 0(int) 表示程序正常执行完毕
 }
 
-
 // With no return, 返回值为 void/空值 类型
 void sayHi() {
 	cout << "Hello xi!\n";// 末尾无需返回值
+}
+
+// // 定义函数，返回值为整型
+// int func(){
+// 	func_body;
+// 	return 0;
+// }
+// // 直接用函数名和参数调用
+// func();
+
+// The only one main function
+int main()
+{	
+	sayHi();
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Nice to meet you, " << name << endl;
+	return 0;
 }
