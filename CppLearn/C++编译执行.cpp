93de156cf841a9380630906ac0af1bbb089720da
oca-john <40886062+oca-john@users.noreply.github.com>
2021-07-19 11:32:15 +0800

    Update and rename C++程序怎样编译执行.cpp to C++编译执行.cpp

--- CppLearn/C++编译执行.cpp
+++ CppLearn/C++编译执行.cpp
@@ -3,7 +3,7 @@
 // source_file.cpp  源码  
 // -o               输出选项  
 // executable_file  可执行文件  
-`g++ source_file.cpp -o executable_file`
+`g++ source_file.cpp -o executable_file`  // 编译源码
 
 // 2.执行：
-`./executable_file`
+`./executable_file`                       // 执行可执行文件
